51d58c9343772e39e2d041a6d9ddd300806297a1
unknown <shakibahmed09112017@gmail.com>
2025-01-12 04:07:42 +0600

    add api gateway

---
+++ film-app-api-gateway/src/test/java/com/example/microservice/FimAppApiGateway/FimAppApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice.FimAppApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FimAppApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
