bbef3b23fcbbc94c0dafb89372ec46f21e1e3256
unknown <shakibahmed09112017@gmail.com>
2025-03-12 01:50:33 +0600

    adding actuator for api gateway and other services

--- customer-service/src/main/java/com/example/microservice/customer_service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/microservice/customer_service/CustomerServiceApplication.java
@@ -1,13 +1,22 @@
 package com.example.microservice.customer_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
+	@Bean
+	public HttpExchangeRepository httpExchangeRepository() {
+		return new InMemoryHttpExchangeRepository();
+	}
 }
