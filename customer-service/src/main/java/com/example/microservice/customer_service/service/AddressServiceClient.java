95ca7e13cf105653ebbf5f90dd325c0fdd19a023
unknown <shakibahmed09112017@gmail.com>
2025-03-16 01:17:54 +0600

    add resilience4j support

--- customer-service/src/main/java/com/example/microservice/customer_service/service/AddressServiceClient.java
+++ customer-service/src/main/java/com/example/microservice/customer_service/service/AddressServiceClient.java
@@ -1,6 +1,8 @@
 package com.example.microservice.customer_service.service;
 
 import com.example.microservice.customer_service.dto.AddressResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,5 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface AddressServiceClient {
 
     @GetMapping("/addresses/getById/{id}")
+    @Retry(name="addressservice")
+    @CircuitBreaker(name="addressservice", fallbackMethod = "getAddressByIdFallback")
     public AddressResponse getAddressById(@PathVariable Integer id);
+
+    default AddressResponse getAddressByIdFallback(Integer id, Throwable exception) {
+        System.out.println("Exception: " + exception.getMessage());
+        System.out.println("Parameter: " + id);
+        return new AddressResponse();
+    }
 }
